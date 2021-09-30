c02e391f1af485de4c44661ef7b06d435e253182
HeeSeok Noh <nhs0912@nate.com>
2021-09-30 21:49:48 +0900

    feat: StringTest 요구사항 3 완성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,7 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StringTest {
 
@@ -17,7 +18,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항 1-2 : 1을 ,로 split하였을 때 분리 되는지 확인하는 테스트")
+    @DisplayName("요구사항 1-2 : 1을 ,로 split 하였을 때 분리 되는지 확인하는 테스트")
     public void divideTest2() {
         String inputText = "1";
         String[] resultTexts = inputText.split(",");
@@ -25,10 +26,31 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항 2 : (1,2)을 substring()method를 활용해 () 제거하고 1,2을 반환하는 테스트")
+    @DisplayName("요구사항 2 : (1,2)을 substring() method를 활용해 () 제거하고 1,2을 반환하는 테스트")
     public void removeBracketTest() {
         String inputText = "(1,2)";
         String result = inputText.substring(1, inputText.indexOf(')'));
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항 3-1 : abc에서 charAt()을 활용해 특정 위치 문자를 찾는 테스트")
+    public void findTextTest() {
+        String inputText = "abc";
+        assertAll(
+                () -> assertThat(inputText.charAt(0)).isEqualTo('a'),
+                () -> assertThat(inputText.charAt(1)).isEqualTo('b'),
+                () -> assertThat(inputText.charAt(2)).isEqualTo('c')
+        );
+    }
+
+    @Test
+    @DisplayName("요구사항 3-2 : abc에서 charAt()을 활용해 허용하는 범위 밖의 문자를 찾는 테스트")
+    public void findTextErrorTest() {
+        String inputText = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    assertThat(inputText.charAt(4)).isEqualTo('a');
+                }).withMessageMatching("String index out of range: 4");
     }
 }
