cb9fa059260ab9143105b2e3ccf3bc96325752da
HeeSeok Noh <nhs0912@nate.com>
2021-09-30 22:52:18 +0900

    feat: SetTest 요구사항 1 완성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항1 : set의 size()를 활용해 set size를 확인하는 테스트")
+    void confirmSizeOfSet() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+}
