b8c8701fa1f70ef815e833ab0dccd0d3ec037e7f
HeeSeok Noh <nhs0912@nate.com>
2021-09-30 21:39:33 +0900

    feat: StringTest 요구사항 2 완성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,18 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    @DisplayName("1,2가 잘 분리 되는지 확인하는 테스트")
-    public void divideTest(){
+    @DisplayName("요구사항 1-1 : 1,2가 잘 분리 되는지 확인하는 테스트")
+    public void divideTest() {
         String inputText = "1,2";
         String[] resultTexts = inputText.split(",");
-        assertThat(resultTexts).contains("1","2");
+        assertThat(resultTexts).contains("1", "2");
     }
 
     @Test
-    @DisplayName("1을 ,로 split하였을 때 분리 되는지 확인하는 테스트")
-    public void divideTest2(){
+    @DisplayName("요구사항 1-2 : 1을 ,로 split하였을 때 분리 되는지 확인하는 테스트")
+    public void divideTest2() {
         String inputText = "1";
         String[] resultTexts = inputText.split(",");
         assertThat(resultTexts).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("요구사항 2 : (1,2)을 substring()method를 활용해 () 제거하고 1,2을 반환하는 테스트")
+    public void removeBracketTest() {
+        String inputText = "(1,2)";
+        String result = inputText.substring(1, inputText.indexOf(')'));
+        assertThat(result).isEqualTo("1,2");
     }
 }
